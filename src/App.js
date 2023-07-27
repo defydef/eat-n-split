6bcc8956162c32929a12c57e0d3ca10065847c0b
defy <8841170+defydef@users.noreply.github.com>
2023-07-27 08:01:34 +0700

    Add static list of friends

--- src/App.js
+++ src/App.js
@@ -24,23 +24,42 @@ function App() {
   return (
     <div className="app">
       <div className="sidebar">
-        <ul>
-          {initialFriends.map((friend) => (
-            <li>
-              <h3>{friend.name}</h3>
-              <p>
-                You owe {friend.name} ${friend.balance}
-              </p>
-            </li>
-          ))}
-        </ul>
+        <FriendList friends={initialFriends} />
+        <button className="button">Add friend</button>
       </div>
     </div>
   );
 }
 
-function FriendList() {}
+function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} />
+      ))}{" "}
+    </ul>
+  );
+}
 
-function Friend({ friend }) {}
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt="friend avatar" />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 ? (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      ) : friend.balance > 0 ? (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      ) : (
+        <p>You and {friend.name} are even</p>
+      )}
+      <button className="button">Select</button>
+    </li>
+  );
+}
 
 export default App;
